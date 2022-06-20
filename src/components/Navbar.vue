56d92782e29321c46c1c6eb4c2d2f9612a7148ee
Nestorgonza <mcclein02@hotmail.com>
2022-06-20 12:19:57 -0500

    Initial commit

---
+++ src/components/Navbar.vue
@@ -0,0 +1,12 @@
+<template lang="">
+	<div class="navbar navbar-dark bg-dark">
+		<router-link to="/" class="navbar-brand"> Formulario </router-link>
+		<div class="d-flex">
+			<router-link to="/" class="btn btn-dark">Tareas </router-link>
+		</div>
+	</div>
+</template>
+<script>
+	export default {};
+</script>
+<style lang=""></style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,50 @@
+<template>
+	<h1 class="my-5">Formulario con Vue.js</h1>
+
+	<form @submit.prevent="procesarFormulario">
+		<Input :tarea="tarea" />
+	</form>
+	<hr />
+	<p>
+		{{ tarea }}
+	</p>
+</template>
+
+<script>
+	import Imput from "../components/Input.vue";
+
+	export default {
+		name: "HomeView",
+		components: {
+			Imput,
+		},
+		data() {
+			return {
+				tarea: {
+					id: "",
+					nombre: "",
+					categorias: [],
+					estado: "",
+					numero: 0,
+				},
+			};
+		},
+		methods: {
+			procesarFormulario() {
+				console.log(this.tarea);
+				if (this.tarea.nombre.trim() === "") {
+					console.log("Esta vacío ");
+					return;
+				}
+				console.log("no está vacío ");
+				// para vaciar los campos
+				this.tarea = {
+					nombre: "",
+					categorias: [],
+					estado: "",
+					numero: 0,
+				};
+			},
+		},
+	};
+</script>
